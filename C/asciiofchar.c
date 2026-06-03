b437c19e88ce83e7cf6dd27d1d8f31f2f7459ae2
AlphinDThomas <alphinprofessional23@gmail.com>
2026-06-03 17:42:43 +0530

    c test

---
+++ C/asciiofchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+  char char1;
+  scanf("%c", &char1);
+
+  printf("%d\n", char1);
+  printf("%c\n", char1 + 1);
+  printf("%c\n", char1 - 1);
+
+  return 0;
+}
